165da2240b25402465749762c3a4f195d5a5fdbe
Dev Ranjan <devranjan68@gmail.com>
2022-02-26 21:14:12 +0530

    Project 1 check-in

---
+++ Program 1/src/mypackage/MyClass.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1;i<6;i++)
+	System.out.println("Hello");
+	}
+
+}
