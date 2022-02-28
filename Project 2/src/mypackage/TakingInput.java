62e77bb4ef49d0200349e08d63fcc2a38659fbff
Dev Ranjan <devranjan68@gmail.com>
2022-02-28 20:50:29 +0530

    Taking user input

--- Project 2/src/mypackage/TakingInput.java
+++ Project 2/src/mypackage/TakingInput.java
@@ -1,19 +1,21 @@
 package mypackage;
 import java.util.Scanner;
-public class MyClass {
+public class TakingInput 
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Taking input from the user");
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter first number");
-		int a=sc.nextInt();
+		float a=sc.nextFloat();
 		//System.out.println(sc.hasNextInt());
-		boolean b1=sc.hasNextInt();
-		System.out.println(b1);
+		//boolean b1=sc.hasNextInt();
+		//System.out.println(b1);
 		System.out.println("Enter second number");
 		int b=sc.nextInt();
 		System.out.println("The sum of two numbers is :\n"+(a+b));
+
 	}
 
 }
