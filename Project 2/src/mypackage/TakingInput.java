c9d5fa38ed7399495357abe38e98da9d4ea7dbc1
Dev Ranjan <devranjan68@gmail.com>
2022-03-01 02:44:06 +0530

    Operators

--- Project 2/src/mypackage/TakingInput.java
+++ Project 2/src/mypackage/TakingInput.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class TakingInput 
 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-		System.out.println("Taking input from the user");
+		//System.out.println("Taking input from the user");
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter first number");
 		float a=sc.nextFloat();
@@ -13,8 +14,9 @@ public class TakingInput
 		//boolean b1=sc.hasNextInt();
 		//System.out.println(b1);
 		System.out.println("Enter second number");
-		int b=sc.nextInt();
-		System.out.println("The sum of two numbers is :\n"+(a+b));
+		float b=sc.nextFloat();
+		System.out.println("The remainder of two numbers is :\n"+(a%b));
+		//System.out.println(sc.next()=="Dev");
 
 	}
 
