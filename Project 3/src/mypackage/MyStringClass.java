ddb45558ba59e609b7e0c300ef87f2e2176d41af
Dev Ranjan <devranjan68@gmail.com>
2022-03-01 23:10:28 +0530

    Play with data types

---
+++ Project 3/src/mypackage/MyStringClass.java
@@ -0,0 +1,20 @@
+package mypackage;
+import java.util.Scanner;
+
+public class MyStringClass 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		//String str = new String("Dev");
+		//System.out.println(str);
+		Scanner sc=new Scanner(System.in);
+		//String str=sc.nextLine();
+		//System.out.printf("The string is:\n%s\n",str);
+		Float b=sc.nextFloat();
+		System.out.format("%3.2f",b);
+
+	}
+
+}
